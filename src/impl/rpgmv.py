798c618307eb73ea87c71cac28f4f2f9d64d787b
xaondxmzrd <xaondxmzrd@gmail.com>
2025-05-09 14:47:04 +0800

    调整顺序

--- src/impl/rpgmv.py
+++ src/impl/rpgmv.py
@@ -14,25 +14,18 @@ def get_first_param(param):
     return param[0]
 
 
-opcode_extractor_table = {
+extractor_table = {
     Code.ShowText: get_first_param,
     Code.ShowChoicess: get_first_param,
 }
 
 
-def for_each_list(head, pred):
-    return [
-        item
-        for event in head["events"][1:]
-        for page in event["pages"]
-        for item in pred(page["list"])
-    ]
-
-
 def extract_opcode(opcode):
     code = opcode["code"]
-    if code in Code and (extract := opcode_extractor_table.get(code)):
-        return extract(opcode["parameters"])
+    param = opcode["parameters"]
+
+    if code in Code and (extract := extractor_table.get(code)):
+        return extract(param)
 
 
 def extract_list(lis):
@@ -46,10 +39,24 @@ def extract_list(lis):
     return result
 
 
+def for_each_list(head, pred):
+    return [
+        item
+        for event in head["events"][1:]
+        for page in event["pages"]
+        for item in pred(page["list"])
+    ]
+
+
 def extract_file(path):
     with open(path, "r", encoding="utf-8") as f:
         head = json.load(f)
         return for_each_list(head, extract_list)
+
+
+def select_files(dir):
+    data_dir = Path(dir) / "data"
+    return [path for path in data_dir.glob("*.json") if can_handle_file(path)]
 
 
 def extract_dir(path):
@@ -70,8 +77,3 @@ def can_handle_file(path):
 
 def can_handle_dir(path):
     return "package.json" in os.listdir(path)
-
-
-def select_files(dir):
-    data_dir = Path(dir) / "data"
-    return [path for path in data_dir.glob("*.json") if can_handle_file(path)]
